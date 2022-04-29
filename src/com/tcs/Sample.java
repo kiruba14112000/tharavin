021da9ea6366d0618e11397b560268657ae9e847
kiruba14112000 <kirubalovely@gmail.com>
2022-04-29 16:06:26 +0530

    tharavin push

---
+++ src/com/tcs/Sample.java
@@ -0,0 +1,22 @@
+package com.tcs;
+
+public class Sample {
+
+	public void test() {
+		System.out.println("abc");
+	}
+	public void test2() {
+		System.out.println("abc1");
+	}
+	public void test3() {
+		System.out.println("abc2");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.test();
+		s.test2();
+		s.test3();
+		
+	}
+
+}
