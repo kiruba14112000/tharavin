772763be11cbcd38bda0bb93009e6e3afd6bd729
kiruba14112000 <kirubalovely@gmail.com>
2022-04-29 16:55:17 +0530

    kiruba changed

--- src/com/tcs/Sample.java
+++ src/com/tcs/Sample.java
@@ -5,18 +5,26 @@ public class Sample {
 	public void test() {
 		System.out.println("abc");
 	}
+
 	public void test2() {
 		System.out.println("abc1");
 	}
+
 	public void test3() {
 		System.out.println("abc2");
 	}
+
+	private void test4() {
+		System.out.println("abc3");
+	}
+
 	public static void main(String[] args) {
 		Sample s = new Sample();
 		s.test();
 		s.test2();
 		s.test3();
-		
+		s.test4();
+
 	}
 
 }
